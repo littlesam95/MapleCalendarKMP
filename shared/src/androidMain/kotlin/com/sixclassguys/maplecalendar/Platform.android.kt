be3fc3fe123dbf55df1745ebf870363f9ad361a4
littlesam95 <littlesam95@naver.com>
2025-12-30 16:04:51 +0900

    [rename] 각 플랫폼의 name 변수 조정(임시)
    - ANDROID, IOS

--- shared/src/androidMain/kotlin/com/sixclassguys/maplecalendar/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/sixclassguys/maplecalendar/Platform.android.kt
@@ -3,7 +3,8 @@ package com.sixclassguys.maplecalendar
 import android.os.Build
 
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+    // override val name: String = "Android ${Build.VERSION.SDK_INT}"
+    override val name: String = "ANDROID"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/iosMain/kotlin/com/sixclassguys/maplecalendar/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/sixclassguys/maplecalendar/Platform.ios.kt
@@ -3,7 +3,8 @@ package com.sixclassguys.maplecalendar
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    // override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val name: String = "IOS"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
