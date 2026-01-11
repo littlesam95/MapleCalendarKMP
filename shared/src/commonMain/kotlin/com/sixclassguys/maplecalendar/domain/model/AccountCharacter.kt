6ed15c978047394c6669bbdd416279baedb66c18
littlesam95 <littlesam95@naver.com>
2026-01-11 23:03:05 +0900

    [feat] DTO, Entity 작성

---
+++ shared/src/commonMain/kotlin/com/sixclassguys/maplecalendar/domain/model/AccountCharacter.kt
@@ -0,0 +1,8 @@
+package com.sixclassguys.maplecalendar.domain.model
+
+data class AccountCharacter(
+    val ocid: String,
+    val characterName: String,
+    val characterClass: String,
+    val characterLevel: Int
+)
