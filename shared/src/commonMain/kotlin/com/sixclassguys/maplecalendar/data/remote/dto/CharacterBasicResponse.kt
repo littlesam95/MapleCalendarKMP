cc7c676bcc40713d79dcc815c5d1a5140fd241b7
littlesam95 <littlesam95@naver.com>
2026-01-18 00:49:44 +0900

    [fix] CharacterBasic의 toDomain()을 데이터 클래스의 내부 함수로 변경

--- shared/src/commonMain/kotlin/com/sixclassguys/maplecalendar/data/remote/dto/CharacterBasicResponse.kt
+++ shared/src/commonMain/kotlin/com/sixclassguys/maplecalendar/data/remote/dto/CharacterBasicResponse.kt
@@ -20,22 +20,23 @@ data class CharacterBasicResponse(
     @SerialName("character_date_create") val characterDateCreate: String? = null,
     @SerialName("access_flag") val accessFlag: String? = null,
     @SerialName("liberation_quest_clear") val liberationQuestClear: String? = null
-)
-
-fun CharacterBasicResponse.toDomain(): CharacterBasic {
-    return CharacterBasic(
-        characterName = this.characterName ?: "김창섭",
-        worldName = this.worldName ?: "테스트월드1",
-        characterGender = this.characterGender ?: "남",
-        characterClass = this.characterClass ?: "초보자",
-        characterClassLevel = (this.characterClassLevel ?: "0").toInt(),
-        characterLevel = this.characterLevel ?: 0L,
-        characterExp = this.characterExp ?: 0L,
-        characterExpRate = (this.characterExpRate ?: "0.0").toDouble(),
-        characterGuildName = this.characterGuildName ?: "6반남",
-        characterImage = this.characterImage ?: "",
-        characterDateCreate = this.characterDateCreate ?: "2003-04-29",
-        characterAccessFlag = this.accessFlag.toBoolean(),
-        characterLiberationQuestClear = (this.liberationQuestClear ?: "0").toInt()
-    )
+) {
+    fun toDomain(): CharacterBasic {
+        return CharacterBasic(
+            characterName = this.characterName ?: "김창섭",
+            worldName = this.worldName ?: "테스트월드1",
+            characterGender = this.characterGender ?: "남",
+            characterClass = this.characterClass ?: "초보자",
+            characterClassLevel = (this.characterClassLevel ?: "0").toInt(),
+            characterLevel = this.characterLevel ?: 0L,
+            characterExp = this.characterExp ?: 0L,
+            characterExpRate = (this.characterExpRate ?: "0.0").toDouble(),
+            characterGuildName = this.characterGuildName ?: "6반남",
+            characterImage = this.characterImage ?: "",
+            characterDateCreate = this.characterDateCreate ?: "2003-04-29",
+            characterAccessFlag = this.accessFlag.toBoolean(),
+            characterLiberationQuestClear = (this.liberationQuestClear ?: "0").toInt()
+        )
+    }
 }
+
