23c07f123694c33fa8bc34c9678329cfe4ed02da
littlesam95 <littlesam95@naver.com>
2026-02-10 16:15:14 +0900

    [feat] 보스 파티 알람 쪽 채널 추가, 알람 수신 시 알람 내역 갱신

--- shared/src/commonMain/kotlin/com/sixclassguys/maplecalendar/domain/repository/NotificationEventBus.kt
+++ shared/src/commonMain/kotlin/com/sixclassguys/maplecalendar/domain/repository/NotificationEventBus.kt
@@ -6,5 +6,9 @@ interface NotificationEventBus {
 
     val events: SharedFlow<Long>
 
+    val bossPartyId: SharedFlow<Long>
+
     suspend fun emitEvent(eventId: Long)
+
+    suspend fun emitBossPartyId(bossPartyId: Long)
 }
